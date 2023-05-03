f751353a8ced3f9f02ff2aad95bf93faaaa6f035
Vili-Petteri Maunu <vilipetterimaunu@gmail.com>
2023-05-03 15:14:06 +0300

    Added landingview component

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom"
+import { LandingView } from "./components/landing-view/LandingView"
 
 function App() {
-  return <Routes>
-    <Route></Route>
-  </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<LandingView />} />
+    </Routes>
+  )
 }
 
 export default App;
