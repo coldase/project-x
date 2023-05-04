d741faab4b789843ad0ba6e7f00fcc0085b90068
Vili-Petteri Maunu <vilipetterimaunu@gmail.com>
2023-05-04 14:46:42 +0300

    Form redo

--- front/src/components/login-view/LoginView.jsx
+++ front/src/components/login-view/LoginView.jsx
@@ -1,43 +1,30 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
 
 const LoginView = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="Username" {...register("Username", {})} />
-      <input
-        type="text"
-        placeholder="First name"
-        {...register("First name", { required: true, maxLength: 80 })}
-      />
-      <input
-        type="text"
-        placeholder="Last name"
-        {...register("Last name", { required: true, maxLength: 100 })}
-      />
-      <input
-        type="text"
-        placeholder="Email"
-        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
-      />
-      <input
-        type="tel"
-        placeholder="Mobile number"
-        {...register("Mobile number", {
-          required: true,
-          minLength: 6,
-          maxLength: 12,
-        })}
-      />
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
 
-      <input type="submit" />
+  const handleLoginClick = (event) => {
+    event.preventDefault();
+    console.log(user, pass);
+  };
+  return (
+    <form onSubmit={handleLoginClick}>
+      <label>username</label>
+      <input
+        type="text"
+        required
+        value={user}
+        onChange={(e) => setUser(e.target.value)}
+      />
+      <label>password</label>
+      <input
+        type="password"
+        required
+        value={pass}
+        onChange={(e) => setPass(e.target.value)}
+      />
+      <button>clic</button>
     </form>
   );
 };
